fe9423fef6c88dee0f83748e3518ccf02df2b63d
Jade <jademfcoelho@gmail.com>
2024-03-28 12:30:57 -0300

    update environment

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://back-catalogo-190cknd2n-jadecoelhos-projects.vercel.app/'
+  apiUrl: 'https://back-catalogo.vercel.app/catalogos'
 };
