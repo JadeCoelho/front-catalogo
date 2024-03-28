9e563bd5bd61210e0d1d287f2303724aab4d2326
Jade <jademfcoelho@gmail.com>
2024-03-28 11:48:17 -0300

    atualizei link do vercel

--- src/app/catalogo.service.ts
+++ src/app/catalogo.service.ts
@@ -12,7 +12,7 @@ export class CatalogoService {
   // url = 'http://localhost:8000/catalogos'
 
   // apiUrl = environment.apiUrl;
-  apiUrl = 'https://back-catalogo.vercel.app'
+  apiUrl = 'https://back-catalogo-190cknd2n-jadecoelhos-projects.vercel.app/catalogos'
 
   constructor(private http: HttpClient){}
 
